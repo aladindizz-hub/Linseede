c55fbee817567a6a9e59fc1353122a672173755f
aladindizz-hub <aladindizz@gmail.com>
2026-06-03 00:41:49 +0300

    Create color_tokens.dart

---
+++ lib/core/theme/tokens/color_tokens.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class ColorTokens {
+  ColorTokens._();
+
+  static const Color luxuryBgPrimary = Color(0xFF0F172A);
+  static const Color luxuryBgSecondary = Color(0xFF131C2E);
+  static const Color luxuryElevated = Color(0xFF1B2740);
+  static const Color luxuryPanel = Color(0xFF202E4A);
+  static const Color luxurySidebar = Color(0xFF182235);
+
+  static const Color luxuryAccentBlue = Color(0xFF38BDF8);
+  static const Color luxuryAccentPurple = Color(0xFF8B5CF6);
+  static const Color luxuryAccentCyan = Color(0xFF22D3EE);
+  static const Color luxuryAccentGreen = Color(0xFF4ADE80);
+  static const Color luxuryWarning = Color(0xFFFBBF24);
+  static const Color luxuryDanger = Color(0xFFFB7185);
+
+  static const Color auroraBgPrimary = Color(0xFF0A0E1A);
+  static const Color auroraBgSecondary = Color(0xFF0F1424);
+  static const Color auroraElevated = Color(0xFF161E33);
+  static const Color auroraPanel = Color(0xFF1B2440);
+  static const Color auroraSidebar = Color(0xFF111726);
+
+  static const Color auroraAccentGreen = Color(0xFF34D399);
+  static const Color auroraAccentTeal = Color(0xFF2DD4BF);
+  static const Color auroraAccentViolet = Color(0xFF7C3AED);
+  static const Color auroraAccentMint = Color(0xFFA7F3D0);
+  static const Color auroraWarning = Color(0xFFFCD34D);
+  static const Color auroraDanger = Color(0xFFF472B6);
+
+  static const Color royalBgPrimary = Color(0xFF11091F);
+  static const Color royalBgSecondary = Color(0xFF170D27);
+  static const Color royalElevated = Color(0xFF231336);
+  static const Color royalPanel = Color(0xFF2A1843);
+  static const Color royalSidebar = Color(0xFF180C2C);
+
+  static const Color royalAccentGold = Color(0xFFF5C26B);
+  static const Color royalAccentMagenta = Color(0xFFE879F9);
+  static const Color royalAccentAmber = Color(0xFFFBBF24);
+  static const Color royalAccentPlum = Color(0xFFC084FC);
+  static const Color royalWarning = Color(0xFFFCA5A5);
+  static const Color royalDanger = Color(0xFFF87171);
+
+  static const Color arcticBgPrimary = Color(0xFFF3F6FB);
+  static const Color arcticBgSecondary = Color(0xFFE9EFF8);
+  static const Color arcticElevated = Color(0xFFFFFFFF);
+  static const Color arcticPanel = Color(0xFFFFFFFF);
+  static const Color arcticSidebar = Color(0xFFE4ECF7);
+
+  static const Color arcticAccentSky = Color(0xFF0EA5E9);
+  static const Color arcticAccentCyan = Color(0xFF06B6D4);
+  static const Color arcticAccentBlue = Color(0xFF2563EB);
+  static const Color arcticAccentMint = Color(0xFF10B981);
+  static const Color arcticWarning = Color(0xFFD97706);
+  static const Color arcticDanger = Color(0xFFE11D48);
+
+  static const Color sakuraBgPrimary = Color(0xFF1C1421);
+  static const Color sakuraBgSecondary = Color(0xFF241829);
+  static const Color sakuraElevated = Color(0xFF2E1F36);
+  static const Color sakuraPanel = Color(0xFF382642);
+  static const Color sakuraSidebar = Color(0xFF21172A);
+
+  static const Color sakuraAccentPink = Color(0xFFF9A8D4);
+  static const Color sakuraAccentCoral = Color(0xFFFB7185);
+  static const Color sakuraAccentRose = Color(0xFFFDA4AF);
+  static const Color sakuraAccentLavender = Color(0xFFC4B5FD);
+  static const Color sakuraWarning = Color(0xFFFCD34D);
+  static const Color sakuraDanger = Color(0xFFEF4444);
+
+  static const Color carbonBgPrimary = Color(0xFF0B0B0F);
+  static const Color carbonBgSecondary = Color(0xFF111116);
+  static const Color carbonElevated = Color(0xFF18181F);
+  static const Color carbonPanel = Color(0xFF1F1F28);
+  static const Color carbonSidebar = Color(0xFF131318);
+
+  static const Color carbonAccentWhite = Color(0xFFFAFAFA);
+  static const Color carbonAccentLime = Color(0xFFA3E635);
+  static const Color carbonAccentSilver = Color(0xFFD1D5DB);
+  static const Color carbonAccentGray = Color(0xFF6B7280);
+  static const Color carbonWarning = Color(0xFFFACC15);
+  static const Color carbonDanger = Color(0xFFEF4444);
+
+  static const Color glassWhite08 = Color(0x14FFFFFF);
+  static const Color glassWhite10 = Color(0x1AFFFFFF);
+  static const Color glassWhite14 = Color(0x24FFFFFF);
+  static const Color glassWhite04 = Color(0x0AFFFFFF);
+
+  static const Color glassBlack08 = Color(0x14000000);
+  static const Color glassBlack10 = Color(0x1A000000);
+  static const Color glassBlack14 = Color(0x24000000);
+
+  static const Color textPrimary = Color(0xFFF1F5F9);
+  static const Color textSecondary = Color(0xFFCBD5E1);
+  static const Color textTertiary = Color(0xFF94A3B8);
+  static const Color textMuted = Color(0xFF64748B);
+
+  static const Color textPrimaryLight = Color(0xFF0F172A);
+  static const Color textSecondaryLight = Color(0xFF334155);
+  static const Color textTertiaryLight = Color(0xFF64748B);
+  static const Color textMutedLight = Color(0xFF94A3B8);
+}
