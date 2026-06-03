3e6fa8ec2d7199655ab206791a7da28ecac250cc
aladindizz-hub <aladindizz@gmail.com>
2026-06-03 05:04:02 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutter/services.dart';
 import 'package:hiddify/bootstrap.dart';
+import 'package:hiddify/core/app/display_mode_setup.dart';
 import 'package:hiddify/core/model/environment.dart';
 
 Future<void> main() async {
   final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   // final widgetsBinding = SentryWidgetsFlutterBinding.ensureInitialized();
   // debugPaintSizeEnabled = true;
+
+  await setupHighRefreshRate();
 
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
   SystemChrome.setSystemUIOverlayStyle(
